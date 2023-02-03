2c49143a33fea772dbc5ded755c1cbfe2a9c7a02
«Maria» <chalova_2111@mail.ru>
2023-02-03 16:28:22 +0500

    ДЗ 2.02.23

---
+++ Task19/Program.cs
@@ -0,0 +1,39 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число и проверяет, 
+//является ли оно палиндромом.
+//14212 -> нет
+//12821 -> да
+//23432 -> да
+
+
+Console.Clear();
+int num, r, sum = 0, t;
+Console.Write("Введите пятизначное число: ");
+num = int.Parse(Console.ReadLine()!);
+
+if (num >= 10000)
+{
+    for (t = num; num != 0; num = num / 10)
+    {
+        r = num % 10;
+        sum = sum * 10 + r;
+    }
+    if (t == sum)
+        Console.Write("{0} палиндром", t);
+    else
+        Console.Write("{0} не палиндромом", t);
+}
+else
+{
+    Console.Write("Вы ввели не пятизначное число: ");
+    num = int.Parse(Console.ReadLine()!);
+
+    for (t = num; num != 0; num = num / 10)
+    {
+        r = num % 10;
+        sum = sum * 10 + r;
+    }
+    if (t == sum)
+        Console.Write("{0} полиондром", t);
+    else
+        Console.Write("{0} не полиондром", t);
+}
---
+++ Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿//Напишите программу, которая принимает на вход координаты двух точек и находит 
+//расстояние между ними в 3D пространстве.
+//A (3,6,8); B (2,1,-7), -> 15.84
+//A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Clear();
+
+Console.Write("Введите координаты точки X1: ");
+int X1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите координаты точки Y1: ");
+double Y1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координаты точки A1: ");
+double A1 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координаты точки X2: ");
+double X2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координаты точки Y2: ");
+double Y2 = Convert.ToDouble(Console.ReadLine()!);
+Console.Write("Введите координаты точки A2: ");
+double A2 = Convert.ToDouble(Console.ReadLine()!);
+
+double S = Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2) + Math.Pow(A1 - A2, 2);
+
+double len = Math.Sqrt(S);
+
+Console.WriteLine($"{len:f2}");
+
+
+
